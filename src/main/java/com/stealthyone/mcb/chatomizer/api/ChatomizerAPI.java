be3c585a5ae13543b847c4f2b08282b27b68d53f
Stealth2800 <stealth2800@stealthyone.com>
2014-02-20 22:36:34 -0800

    Updated createChatEvent method to use Chatters instead of Players

--- src/main/java/com/stealthyone/mcb/chatomizer/api/ChatomizerAPI.java
+++ src/main/java/com/stealthyone/mcb/chatomizer/api/ChatomizerAPI.java
@@ -20,7 +20,6 @@ package com.stealthyone.mcb.chatomizer.api;
 
 import com.stealthyone.mcb.chatomizer.ChatomizerPlugin;
 import com.stealthyone.mcb.chatomizer.api.chatters.Chatter;
-import com.stealthyone.mcb.chatomizer.api.chatters.ChatterPlayer;
 import com.stealthyone.mcb.chatomizer.api.events.AsyncPlayerMultiChatEvent;
 import com.stealthyone.mcb.chatomizer.backend.PlayerManager;
 import com.stealthyone.mcb.chatomizer.backend.formats.FormatManager;
@@ -28,7 +27,6 @@ import com.stealthyone.mcb.chatomizer.config.ConfigHelper;
 import com.stealthyone.mcb.chatomizer.permissions.PermissionNode;
 import com.stealthyone.mcb.chatomizer.utils.ChatColorUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -60,33 +58,30 @@ public final class ChatomizerAPI {
         return ChatomizerPlugin.getInstance().getModifierManager().unregisterModifier(chatModifier);
     }
 
-    public static AsyncPlayerMultiChatEvent createChatEvent(Player sender, String message, Set<Player> recipients) {
+    public static AsyncPlayerMultiChatEvent createChatEvent(Chatter sender, String message, Set<Chatter> recipients) {
         ChatomizerPlugin plugin = ChatomizerPlugin.getInstance();
 
         FormatManager formatManager = plugin.getFormatManager();
         PlayerManager playerManager = plugin.getPlayerManager();
 
-        Chatter senderChatter = new ChatterPlayer(sender);
-
         Map<Chatter, ChatFormat> recipientFormats = new HashMap<>();
-        recipientFormats.put(senderChatter, senderChatter.getChatFormat());
+        recipientFormats.put(sender, sender.getChatFormat());
 
         boolean sendToConsole = ConfigHelper.LOG_CHAT.get();
         ChatFormat consoleFormat = formatManager.getFormat(ConfigHelper.CONSOLE_CHAT_FORMAT.get());
 
-        for (Player recipient : recipients) {
-            Chatter recipientChatter = new ChatterPlayer(recipient);
-            recipientFormats.put(recipientChatter, recipientChatter.getChatFormat());
+        for (Chatter recipient : recipients) {
+            recipientFormats.put(recipient, recipient.getChatFormat());
         }
 
-        if (PermissionNode.CHAT_COLOR.isAllowed(sender))
+        if (sender.hasPermission(PermissionNode.CHAT_COLOR.getPermission()))
             message = ChatColorUtils.colorizeMessage(message);
-        if (PermissionNode.CHAT_FORMATTING.isAllowed(sender))
+        if (sender.hasPermission(PermissionNode.CHAT_FORMATTING.getPermission()))
             message = ChatColorUtils.formatMessage(message);
-        if (PermissionNode.CHAT_MAGIC.isAllowed(sender))
+        if (sender.hasPermission(PermissionNode.CHAT_MAGIC.getPermission()))
             message = ChatColorUtils.magicfyMessage(message);
 
-        AsyncPlayerMultiChatEvent multiChatEvent = new AsyncPlayerMultiChatEvent(senderChatter, message, recipientFormats, sendToConsole, consoleFormat);
+        AsyncPlayerMultiChatEvent multiChatEvent = new AsyncPlayerMultiChatEvent(sender, message, recipientFormats, sendToConsole, consoleFormat);
 
         Bukkit.getPluginManager().callEvent(multiChatEvent);
 
